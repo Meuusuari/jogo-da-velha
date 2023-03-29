0936d6bcd4f81544dc8916d651d05c06cc8f9c0a
danilo barbosa <danilosnascimento2000@gmail.com>
2023-03-29 19:45:41 -0300

    ten commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import TicTacToe from './tictactoe/tictactoe'
+
+function App() {
+  
+
+  return (
+    <div className="App">
+     <TicTacToe/>
+    </div>
+  );
+}
+
+export default App
